35170be9b45decd3cba58e25689948ac4cc919eb
codeinja <rahulsalagundi@outlook.com>
2023-11-17 16:54:36 +0530

    delete changes

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.get("/todos/:id", async (req, res) => {
 
 app.put("/todos/:id", async (req, res) => {
   try {
-    const id  = req.params;
+    const { id }  = req.params;
     const { description } = req.body;
     const updateTodo = await pool.query(
       "UPDATE todo SET description = $1 WHERE todo_id = $2",
@@ -59,7 +59,7 @@ app.put("/todos/:id", async (req, res) => {
 
 app.delete("/todos/:id", async (req, res) => {
   try {
-    const id  = req.params;
+    const { id } = req.params;
     const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id= $1", [id,]);
     res.json("Todo was DELETED");
   } catch (error) {
